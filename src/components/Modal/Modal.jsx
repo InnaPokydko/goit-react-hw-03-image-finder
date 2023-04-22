e885cd443207f152a96939085957b65932c29578
Inna Pokydko <inna.pokydko@gmail.com>
2023-04-22 21:36:53 +0300

    update modal not help

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,37 +1,73 @@
+import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
-import { Overlay,  ModalImg } from './Modal.styled';
+import { Overlay, ModalImg } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-const Modal = ({ onCloseModal, largeImage }) => {
-  const handleKeyDown = (e) => {
+class Modal extends Component {
+  
+  handleKeyDown = (e) => {
     if (e.code === 'Escape') {
-      onCloseModal();
+      this.props.onCloseModal();
     }
   };
 
-  const handleBackdropClick = (e) => {
+  handleBackdropClick = (e) => {
     if (e.currentTarget === e.target) {
-      onCloseModal();
+      this.props.onCloseModal();
     }
   };
 
-  return createPortal(
-    <>
-      <Overlay onClick={handleBackdropClick} onKeyDown={handleKeyDown}>
+  render() {
+    return createPortal(
+      <Overlay onClick={this.handleBackdropClick} onKeyDown={this.handleKeyDown}>
         <ModalImg>
-          <img src={largeImage} alt="" />
+          <img src={this.props.largeImage} alt={this.props.tags} />
         </ModalImg>
-      </Overlay>
-    </>,
-    modalRoot
-  );
-};
+      </Overlay>,
+      modalRoot
+    );
+  }
+}
 
 export default Modal;
 
 Modal.propTypes = {
-  closeModal: PropTypes.func.isRequired,
-  largeImage: PropTypes.string.isRequired,
-};
+  onCloseModal: PropTypes.func.isRequired,
+    largeImage: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+}
+
+
+
+
+// import { createPortal } from 'react-dom';
+// import PropTypes from 'prop-types';
+// import { Overlay,  ModalImg } from './Modal.styled';
+
+// const modalRoot = document.querySelector('#modal-root');
+
+// const Modal = ({ onCloseModal, largeImage, tags }) => {
+//   const handleKeyDown = (e) => {
+//     if (e.code === 'Escape') {
+//       onCloseModal();
+//     }
+//   };
+
+//   const handleBackdropClick = (e) => {
+//     if (e.currentTarget === e.target) {
+//       onCloseModal();
+//     }
+//   };
+
+//   return createPortal(
+
+//       <Overlay onClick={handleBackdropClick} onKeyDown={handleKeyDown}>
+//         <ModalImg>
+//           <img src={largeImage} alt={tags} />
+//         </ModalImg>
+//       </Overlay>,
+//     modalRoot
+//   );
+// };
